3e298dea3eb74149c93e2a11938c361c13fdf32e
Pranay Jagtap <pranayjagtap@gmail.com>
2017-07-26 00:56:29 +0530

    Signed-off-by: Pranay Jagtap <pranayjagtap@gmail.com>

---
+++ test1/src/test1/Threadtest.java
@@ -0,0 +1,26 @@
+package test1;
+
+public class Threadtest extends Thread {
+	
+	public static void main(String args[]) {
+		
+		Threadtest R1 = new Threadtest();
+		R1.start();
+		
+		
+	}
+	public void run() {
+		while(true) {
+		System.out.println("3");
+		calling();
+		}
+		
+		
+		
+	}
+	public static void calling() {
+		
+		System.exit(0);
+	}
+
+}
