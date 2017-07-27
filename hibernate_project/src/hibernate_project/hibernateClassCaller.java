b5da574829929db1b76ee0449d788dff011ce53d
pranayjagtap <pranayjagtap@gmail.com>
2017-07-27 12:50:19 +0530

    Update hibernateClassCaller.java

--- hibernate_project/src/hibernate_project/hibernateClassCaller.java
+++ hibernate_project/src/hibernate_project/hibernateClassCaller.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-//comment
+//comment lol
 import hibernate_project.dto.flex_details;
 
 public class hibernateClassCaller {
